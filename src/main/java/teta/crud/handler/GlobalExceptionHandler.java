07377272576997ee57c95ce83d372c51bd10fc41
lenarsharipov <lenarsharipov@gmail.com>
2023-06-04 19:24:48 +0300

    UPLOAD EXCEPTIONS HANDLER

---
+++ src/main/java/teta/crud/handler/GlobalExceptionHandler.java
@@ -0,0 +1,63 @@
+package teta.crud.handler;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+@Slf4j
+@AllArgsConstructor
+public class GlobalExceptionHandler {
+    private final ObjectMapper objectMapper;
+
+    @ExceptionHandler(value = {NullPointerException.class})
+    public void handleException(Exception exception,
+                                HttpServletRequest request,
+                                HttpServletResponse response) throws IOException {
+        response.setStatus(HttpStatus.BAD_REQUEST.value());
+        response.setContentType("application/json");
+        response.getWriter().write(objectMapper.writeValueAsString(new HashMap<>() { {
+            put("message", "Some of fields empty");
+            put("details", exception.getMessage());
+        }}));
+        log.error(exception.getMessage());
+    }
+
+    @ExceptionHandler(value = {MethodArgumentNotValidException.class})
+    public ResponseEntity<?> argumentNotValidExceptionHandler(MethodArgumentNotValidException e) {
+        return ResponseEntity.badRequest().body(
+                e.getFieldErrors().stream()
+                        .map(f -> Map.of(
+                                f.getField(),
+                                String.format("%s. Actual value: %s",
+                                        f.getDefaultMessage(), f.getRejectedValue())
+                        ))
+                        .collect(Collectors.toList())
+        );
+    }
+
+    @ExceptionHandler(value = {NoSuchElementException.class})
+    public void additionalExceptionHandler(Exception e,
+                                           HttpServletRequest request,
+                                           HttpServletResponse response) throws IOException {
+        response.setStatus(HttpStatus.BAD_REQUEST.value());
+        response.setContentType("application/json");
+        response.getWriter().write(objectMapper.writeValueAsString(new HashMap<>() { {
+            put("message", e.getMessage());
+            put("type", e.getClass());
+        }}));
+    }
+}
