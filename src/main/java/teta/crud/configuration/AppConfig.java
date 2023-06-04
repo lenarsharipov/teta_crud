bd808aedbbdbf8aaeab115017f541d3d7de23342
lenarsharipov <lenarsharipov@gmail.com>
2023-06-04 19:24:28 +0300

    UPLOAD CONFIGS

---
+++ src/main/java/teta/crud/configuration/AppConfig.java
@@ -0,0 +1,26 @@
+package teta.crud.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import teta.crud.repository.CustomerRepository;
+import org.springframework.context.annotation.Bean;
+
+import teta.crud.service.impl.*;
+import teta.crud.service.Searcher;
+
+import java.util.List;
+
+@Configuration
+public class AppConfig {
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Bean
+    public List<Searcher<?>> searchers() {
+        return List.of(
+                new SearcherByName(customerRepository),
+                new SearcherByNumber(customerRepository),
+                new SearchByVehiclePlate(customerRepository)
+        );
+    }
+}
