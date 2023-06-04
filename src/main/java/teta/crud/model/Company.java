7ed253a5788b027748816e2cc03c9b4900734cde
lenarsharipov <lenarsharipov@gmail.com>
2023-06-04 19:20:54 +0300

    UPLOAD MODELS

---
+++ src/main/java/teta/crud/model/Company.java
@@ -0,0 +1,30 @@
+package teta.crud.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "companies")
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Company {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Integer id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "phone", nullable = false)
+    private String phone;
+
+    @Column(name = "address", nullable = false)
+    private String address;
+}
---
+++ src/main/java/teta/crud/model/Customer.java
@@ -0,0 +1,39 @@
+package teta.crud.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "customers")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Integer customerNumber;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "surname", nullable = false)
+    private String surname;
+
+    @Column(name = "vehicle_plate", nullable = false, unique = true)
+    @EqualsAndHashCode.Include
+    private String vehiclePlate;
+
+    @ManyToOne
+    @JoinColumn(
+            name = "company_id",
+            foreignKey = @ForeignKey(name = "COMPANY_ID_FK"),
+            nullable = false
+    )
+    private Company company;
+}
